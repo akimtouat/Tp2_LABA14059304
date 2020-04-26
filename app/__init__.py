0a1696aff514f96aa8d9b52ba5bbacd29f8b9dc2
akimtouat <akim.l.touat@gmail.com>
2020-04-25 22:27:46 -0400

    6 commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
 app.config.from_object('config')
 db = SQLAlchemy(app)
 app.secret_key = "pioupiou"
+, static_url_path="", static_folder="static"
